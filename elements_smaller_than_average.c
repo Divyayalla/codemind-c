d0ea66170f34ba28ac0de92c19015963bc4df3b9
yalla sri surya divya <97617593+Divyayalla@users.noreply.github.com>
2022-06-02 22:07:22 +0530

    #include<stdio.h> int main() {     int c=0,avg,arr[100],sum=0,n;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);         sum+=arr[i];     }     avg=sum/n+1;     for(int i=0;i<n;i++)     {         if(arr[i]<avg)         {             c++;         }     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+int main()
+{
+    int c=0,avg,arr[100],sum=0,n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    avg=sum/n+1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
