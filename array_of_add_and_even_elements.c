e21b75bfe3b2c422e1eb138ab02a318ab0270f94
yalla sri surya divya <97617593+Divyayalla@users.noreply.github.com>
2022-06-02 21:54:44 +0530

    #include<stdio.h> int main() {     int i,n,ar[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         if(ar[i]%2!=0)         printf("%d ",ar[i]);     }     for(i=0;i<n;i++)     {         if(ar[i]%2==0)         printf("%d ",ar[i]);     } }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,21 @@
+ 
+#include<stdio.h>
+int main()
+{
+    int i,n,ar[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]%2!=0)
+        printf("%d ",ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]%2==0)
+        printf("%d ",ar[i]);
+    }
+}
